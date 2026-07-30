efbcf90ede2964f8a1eab7fe43ad069e64825ab2
VinicioAyalaC <vhac2311@gmail.com>
2026-07-29 22:35:36 -0500

    Parte 3: Constructor

--- src/Taller/Aliens/Clases/Alien.java
+++ src/Taller/Aliens/Clases/Alien.java
@@ -3,7 +3,7 @@ package Taller.Aliens.Clases;
 public class Alien {
 	
 	
-	// Atributos
+	// Atributos variables
 	private int tamanio;
 	private String color;
 	private int numeroOjos;
@@ -14,10 +14,42 @@ public class Alien {
 	private double precioCuerpo;
 	
 	
+	// Atributos estáticos
+	private static final int 	TAMANIOMINIMO = 5;
+	private static final int 	TAMANIOMAXIMO = 30;
+	private static final double PORCENTAJECUERPO = 0.20;
+	private static final double	PORCENTAJEEXTREMIDAD = 0.10;
+	private static final double PORCENTAJEOJOS = 0.05;	
+	
+	
+	
+	
+	// constructores
+	public Alien(int tamanio, String color) {
+		
+		if(tamanio <= TAMANIOMINIMO) {
+			tamanio = TAMANIOMINIMO;
+		}else if(tamanio >= TAMANIOMAXIMO ) {
+			tamanio = TAMANIOMAXIMO;
+		}		
+		
+		this.tamanio = tamanio;
+		this.color = color;
+		
+		this.numeroOjos   = 0;
+		this.numeroBrazos = 0;
+		this.numeroPies   = 0;
+		
+		this.precioCuerpo = this.tamanio * PORCENTAJECUERPO;
+		this.precioExtremidad = this.tamanio * PORCENTAJEEXTREMIDAD;
+		this.precioOjo = this.tamanio * PORCENTAJEOJOS;
+		
+	}//fin constructor
+
 	
 
 	
 	
 	
 
-}
+}//principal
